b6d05ca1e781ef0218e20f891b7262c77d26084b
marcushoja0108 <marcushj@live.no>
2024-11-15 15:00:37 +0100

    Add project files.

---
+++ WhatEvenIsThis/Program.cs
@@ -0,0 +1,24 @@
+﻿var range = 250;
+var counts = new int[range];
+string text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine();
+    foreach (var character in text.ToLower() ?? string.Empty)
+    {
+        counts[(int)character]++;
+    }
+
+    for (var i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            double count = (double)counts[i];
+            double percent = count / text.Length * 100;
+            var character = (char)i;
+
+            Console.WriteLine(character + " - " + counts[i]);
+            Console.WriteLine($"{percent}%");
+        }
+    }
+}
